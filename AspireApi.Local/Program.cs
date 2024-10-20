5334b1a74f81244798d0d0d260425489f10e8a3b
BDB <3292107+zero8urn@users.noreply.github.com>
2024-10-19 21:09:06 -0500

    cleanup

--- AspireApi.Local/Program.cs
+++ AspireApi.Local/Program.cs
@@ -4,11 +4,6 @@ var builder = Registrations.BuildHost();
 
 var build = builder.Build();
 
-//var dbResource = (PostgresServerResource)builder.Resources.First(x => x.Name == Registrations.DB);
-
-//var dbService = builder.CreateResourceBuilder(dbResource);
-//var b = dbResource.GetEndpoint("tcp");
-
 await build.RunAsync();
 
 namespace AspireApi.Local
